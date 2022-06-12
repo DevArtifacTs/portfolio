3bbd364ee954208c6be7bf46ca442c61fa82dcc4
RaThomBoi <jessada.srm@gmail.com>
2022-06-12 12:05:03 +0700

    feat && docs && refactor : initialize BackToTop.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+// Test 
+import BackToTop from './components/BackToTop/BackToTop';
+
 function App() {
   return (
     <>
-      
+      <BackToTop />
     </>
   );
 }
---
+++ src/components/BackToTop/BackToTop.jsx
@@ -0,0 +1,39 @@
+import { BackTop } from 'antd';
+import React from 'react';
+import styles from './BackToTop.module.css';
+
+// react icon
+import { IoIosArrowDropup } from 'react-icons/io';
+
+// const style = {
+//   height: 40,
+//   width: 40,
+//   lineHeight: '40px',
+//   borderRadius:  'var(--secondary-border-radius)',
+//   backgroundColor: '#1088e9',
+//   color: '#fff',
+//   textAlign: 'center',
+//   fontSize: 14,
+// };
+
+const BackToTop = () => (
+  <div
+    style={{
+      height: '600vh',
+      padding: 8,
+    }}
+  >
+    <div>Scroll to bottom</div>
+    <div>Scroll to bottom</div>
+    <div>Scroll to bottom</div>
+    <div>Scroll to bottom</div>
+    <div>Scroll to bottom</div>
+    <div>Scroll to bottom</div>
+    <div>Scroll to bottom</div>
+    <BackTop   visibilityHeight={300} duration={200} >
+      <div className={`${styles.to_top_btn}`}  >UP </div>
+    </BackTop>
+  </div>
+);
+
+export default BackToTop;
